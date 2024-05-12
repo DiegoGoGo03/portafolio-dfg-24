a47968dbca9bced9813c577bcb3c8c177b53200a
Diego Gomez <diegofelipegogo03@gmail.com>
2024-05-12 13:46:03 -0500

    Mi rpimer prueba de GIT

---
+++ MANTENIMIENTO.md
@@ -0,0 +1,15 @@
+# My CV Oroject
+Este proyecto es la presentación de mi portafolio
+
+# Prerequisites
+- Acceso a internet
+- Contar con un dispositivo movil, computador o laptop
+
+# Project Structure
+
+El proyecto cuanta con una dubdivisión tradicional:
+1. Header.
+2. Navigation.
+3. Section.
+4. Aside.
+5. Footer.
---
+++ assets/index.js
@@ -0,0 +1,14 @@
+const menu = document.getElementById('menu')
+const btnOpen = document.getElementById('btnOpen')
+const btnClose = document.getElementById('btnClose')
+
+function openMenu() {
+  menu.classList.add('show-menu')
+}
+
+function closeMenu() {
+  menu.classList.remove('show-menu')
+}
+
+btnOpen.onclick = openMenu
+btnClose.onclick = closeMenu

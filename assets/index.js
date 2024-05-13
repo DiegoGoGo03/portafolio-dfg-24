2b4c3e21c3df0f262511886e6fd9fa07d4605078
Diego Gomez <diegofelipegogo03@gmail.com>
2024-05-13 17:25:48 -0500

    Se agrega botón para subir arriba o Scroll Up

--- assets/index.js
+++ assets/index.js
@@ -12,3 +12,24 @@ function closeMenu() {
 
 btnOpen.onclick = openMenu
 btnClose.onclick = closeMenu
+
+
+
+//BOTON DE IR ARRIBA
+window.onscroll = function () {
+  if(document.documentElement.scrollTop > 100) {
+    document.querySelector('.go-top-container').classList.add('show');
+  } else {
+    document.querySelector('.go-top-container').classList.remove('show');
+
+  }
+}
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+  window.scrollTo ({
+    top: 0,
+    behavior: 'smooth'
+  });
+})
+
+
